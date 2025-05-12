a4cfd18e389b7c0aa10a24e8d6ffb3f50a842cc5
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-12 07:15:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var isDate = function (input) {
-  //   write your code here
+  if (input instanceof Date) {
+    return !isNaN(input.valueOf());
+  }
+  // Otherwise, try to parse it
+  return !isNaN(Date.parse(input));
 };
 
 // Do not change the code below.
